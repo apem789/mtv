7b43a63e3c6d6b346f310e19afe62de20e2a7d25
apem <319284737@qq.com>
2020-11-12 21:59:28 +0800

    add top-bar component

---
+++ src/components/public/ComTopBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <van-row class="com-top-bar" type="flex">
+    <van-col>2</van-col>
+    <van-col>3</van-col>
+    <van-col>4</van-col>
+  </van-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '',
+})
+</script>
+
+<style lang="scss" scoped></style>
